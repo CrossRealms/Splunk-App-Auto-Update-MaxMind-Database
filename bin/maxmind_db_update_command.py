e7c229fee0de6107220f0dcb8cd7e930f7045ed9
Vatsal Jagani <vatsaljagani85@gmail.com>
2021-03-24 19:58:43 +0530

    Issues fixed while testing. App seem to be working now.

--- bin/maxmind_db_update_command.py
+++ bin/maxmind_db_update_command.py
@@ -18,7 +18,7 @@ MMDB_PATH_DIR = 'mmdb'
 MMDB_FILE_NAME = 'GeoLite2-City.mmdb'
 
 import splunk.appserver.mrsparkle.lib.util as splunk_lib_util
-APP_LOCAL_PATH = splunk_lib_util.make_splunkhome_path(["etc", "apps", mmdb_utils.APP, "local"])
+APP_LOCAL_PATH = splunk_lib_util.make_splunkhome_path(["etc", "apps", mmdb_utils.APP_NAME, "local"])
 DB_DIR_PATH = os.path.join(APP_LOCAL_PATH, MMDB_PATH_DIR)
 DB_PATH_TO_SET = os.path.join(DB_DIR_PATH, MMDB_FILE_NAME)
 
@@ -36,10 +36,9 @@ class UpdateMaxMindDatabase(GeneratingCommand):
         current_location = '/opt/splunk/share/'
 
         sessionKey = self.search_results_info.auth_token
-        endpoint = '/servicesNS/nobody/{}/configs/conf-{}/{}'.format(mmdb_utils.APP, 'limits', LIMITS_CONF_STANZA)
+        endpoint = '/servicesNS/nobody/{}/configs/conf-{}'.format(mmdb_utils.APP_NAME, 'limits')
         response_status, response_content = rest.simpleRequest(endpoint,
                 sessionKey=sessionKey, getargs={'output_mode':'json'}, raiseAllErrors=True)
-        # TODO - Need to check this
         data = json.loads(response_content)['entry']
         for i in data:
             if i['name'] == LIMITS_CONF_STANZA:
@@ -56,26 +55,25 @@ class UpdateMaxMindDatabase(GeneratingCommand):
         # Update location in limits.conf
         sessionKey = self.search_results_info.auth_token
         postargs = {
+            'name': LIMITS_CONF_STANZA,
             LIMITS_CONF_PARAMETER: DB_PATH_TO_SET
         }
-        endpoint = '/servicesNS/nobody/{}/configs/conf-{}/{}'.format(mmdb_utils.APP, 'limits', LIMITS_CONF_STANZA)
+        endpoint = '/servicesNS/nobody/{}/configs/conf-{}'.format(mmdb_utils.APP_NAME, 'limits')
         response_status, response_content = rest.simpleRequest(endpoint,
                 sessionKey=sessionKey, getargs={'output_mode':'json'}, postargs=postargs, method='POST', raiseAllErrors=True)
-        # TODO - check if the path is updated or not
 
 
     def download_mmdb_database(self, license_key):
         r = requests.get(MaxMindDatabaseDownloadLink.format(license_key), allow_redirects=True)
         open(DB_PATH_TO_SET, 'wb').write(r.content)
-        # Check if the file is saved
 
  
     def generate(self):
         # Create necessary directory is not exist
         if not os.path.exists(APP_LOCAL_PATH):
-            os.makedir(APP_LOCAL_PATH)
+            os.makedirs(APP_LOCAL_PATH)
         if not os.path.exists(DB_DIR_PATH):
-            os.makedir(DB_DIR_PATH)
+            os.makedirs(DB_DIR_PATH)
 
         # Read MaxMind license key
         license_key = self.get_max_mind_license_key()
