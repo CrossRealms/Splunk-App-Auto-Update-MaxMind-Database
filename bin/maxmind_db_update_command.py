e26d36cdcdc7a8a75f00cedb7b34e145e5dc460c
Vatsal Jagani <vatsaljagani85@gmail.com>
2022-10-14 22:14:16 +0530

    Added custom command with savedsearch to test in SHC.

---
+++ bin/maxmind_db_update_command.py
@@ -0,0 +1,21 @@
+import sys
+
+from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration
+import mmdb_utils
+
+
+@Configuration(type='reporting')
+class UpdateMaxMindDatabase(GeneratingCommand):
+
+    def generate(self):
+        try:
+            mmdb_utils.MaxMindDatabaseUtil(self.search_results_info.auth_token)
+
+            # Return Success Message
+            yield {"Message": "Max Mind Database updated successfully."}
+
+        except Exception as e:
+            yield {"Message": "{}".format(e)}
+
+
+dispatch(UpdateMaxMindDatabase, sys.argv, sys.stdin, sys.stdout, __name__)
