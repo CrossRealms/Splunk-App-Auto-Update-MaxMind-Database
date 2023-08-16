62cbcf0acf366cd35894a8fdba5ce65157ba1334
SplunkAppAction <splunkappaction@gmail.com>
2023-08-16 16:48:21 +0000

    splunk_app_action_a15a6bdff7d0a0b4c90b35018c7ff627

--- bin/logger_manager.py
+++ bin/logger_manager.py
@@ -1,3 +1,7 @@
+# This file is generated and maintained by splunk-app-action (https://github.com/VatsalJagani/splunk-app-action)
+# To modify anything create Pull Request on the splunk-app-action GitHub repository.
+
+
 # Standard library imports
 import logging
 import logging.handlers
@@ -5,6 +9,8 @@ import os
 
 # Splunk imports
 from splunk.clilib.bundle_paths import make_splunkhome_path
+
+log_file_prefix = 'auto_update_maxmind_db'
 
 
 def setup_logging(log_name, log_level=logging.INFO):
@@ -14,13 +20,14 @@ def setup_logging(log_name, log_level=logging.INFO):
     :param log_level: log level, a string
     :return: a logger object
     """
-    log_name = 'auto_update_maxmind_db_{}'.format(log_name)
+    log_name = '{}_{}'.format(log_file_prefix, log_name)
     # Make path till log file (current dir (app/<app-name>/bin))
     # log_dir = os.path.dirname(os.path.abspath(__file__))
     # log_file = os.path.join(log_dir, "%s.log" % log_name)
 
     # Make path till log file (splunk/var/log/splunk dir)
-    log_file = make_splunkhome_path(["var", "log", "splunk", "%s.log" % log_name])
+    log_file = make_splunkhome_path(
+        ["var", "log", "splunk", "%s.log" % log_name])
     log_dir = os.path.dirname(log_file)
 
     # Create directory at the required path to store log file, if not found
@@ -41,7 +48,7 @@ def setup_logging(log_name, log_level=logging.INFO):
             log_file, mode="a", maxBytes=10485760, backupCount=10)
         # Format logs
         fmt_str = "%(asctime)s %(levelname)s %(thread)d - %(message)s"
-        formatter = logging.Formatter(fmt_str)
+        formatter = logging.Formatter(fmt_str, datefmt="%Y-%m-%d %H:%M:%S %z")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
